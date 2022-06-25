23b3e2fdbd0b6a1fec72b39b54f1f852f26b04be
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2022-06-25 19:37:18 -0300

    add new module progress

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -17,6 +17,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CurrencyMaskModule } from "ng2-currency-mask";
 import { MatRadioModule } from '@angular/material/radio';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 @NgModule({
   exports: [
     MatTableModule,
@@ -34,8 +35,8 @@ import { MatRadioModule } from '@angular/material/radio';
     MatDividerModule,
     MatCheckboxModule,
     CurrencyMaskModule,
-    MatRadioModule
-
+    MatRadioModule,
+    MatProgressBarModule
   ],
 })
 export class AppMaterialModule {}
