4ff26d70d030a299e5ff45d6e64d941a10fc81ee
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2022-11-26 16:37:26 -0300

    correcao json url

--- src/app/fincon/novo/novo.component.ts
+++ src/app/fincon/novo/novo.component.ts
@@ -8,7 +8,7 @@ import {
   listaParcelas,
   listaTipoLancamentos,
   listaTipoPagamentos,
-  _formatData
+  _formatData,
 } from 'src/app/shared/Util';
 import { ModelComboBox } from '../model/ModelComboBox';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
@@ -18,6 +18,7 @@ import { LancamentoService } from '../services/lancamento.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Lancamento } from '../model/Lancamento';
 import { LocalStorageService } from '../services/local-storage.service';
+import { LancamentoEdit } from '../services/LancamentoEdit.service';
 
 @Component({
   selector: 'app-novo',
@@ -66,8 +67,9 @@ export class NovoComponent implements OnInit {
     private route: ActivatedRoute,
     private serviceLS: LocalStorageService,
     private router: Router,
+    private lancamentoEdit: LancamentoEdit
   ) {
-    const { mes, ano }  = getMesAnoAtual();
+    const { mes, ano } = getMesAnoAtual();
     this.form = this.formBuilder.group({
       descricao: [],
       categoria: [13],
@@ -91,38 +93,37 @@ export class NovoComponent implements OnInit {
       this.router.navigate([''], { relativeTo: this.route });
     }
 
-    const routeParams = this.route.snapshot.paramMap;
-    var paramLancamento = routeParams.get('lancamento');
-    if (paramLancamento) {
-      const param = JSON.parse(paramLancamento);
-      if (param) {
-        this.lancamento = param;
-        this.form = this.formBuilder.group({
-          id: [this.lancamento?.id],
-          descricao: [this.lancamento?.descricao],
-          categoria: [findTipo(this.lancamento?.categoria, this.categorias)],
-          valor: [this.lancamento?.valor],
-          pago: [this.lancamento?.pago],
-          tipo_lancamento: [
-            findTipo(this.lancamento?.tipo_lancamento, this.lancamentos),
-          ],
-          tipo_pagamento: [
-            findTipo(this.lancamento?.tipo_pagamento, this.tipoPagamentos),
-          ],
-          quantidade_parcelas: [this.lancamento?.quantidade_parcelas],
-          mensal: [this.lancamento?.mensal],
-          mes_referencia: [this.lancamento?.mes_referencia],
-          ano_referencia: [this.lancamento?.ano_referencia],
-          observacao: [this.lancamento?.observacao],
+    this.lancamentoEdit
+      .getItems()
+      .map((lancamento) => (this.lancamento = lancamento));
 
-          data_vencimento: [formatDataInput(this.lancamento?.data_vencimento)],
-          data_prevista_pagamento: [
-            formatDataInput(this.lancamento?.data_prevista_pagamento),
-          ],
-        });
-        this.functionSelectTipoPagamento(this.lancamento.tipo_pagamento);
-      }
+    if (this.lancamento) {
+      this.form = this.formBuilder.group({
+        id: [this.lancamento?.id],
+        descricao: [this.lancamento?.descricao],
+        categoria: [findTipo(this.lancamento?.categoria, this.categorias)],
+        valor: [this.lancamento?.valor],
+        pago: [this.lancamento?.pago],
+        tipo_lancamento: [
+          findTipo(this.lancamento?.tipo_lancamento, this.lancamentos),
+        ],
+        tipo_pagamento: [
+          findTipo(this.lancamento?.tipo_pagamento, this.tipoPagamentos),
+        ],
+        quantidade_parcelas: [this.lancamento?.quantidade_parcelas],
+        mensal: [this.lancamento?.mensal],
+        mes_referencia: [this.lancamento?.mes_referencia],
+        ano_referencia: [this.lancamento?.ano_referencia],
+        observacao: [this.lancamento?.observacao],
+
+        data_vencimento: [formatDataInput(this.lancamento?.data_vencimento)],
+        data_prevista_pagamento: [
+          formatDataInput(this.lancamento?.data_prevista_pagamento),
+        ],
+      });
+      this.functionSelectTipoPagamento(this.lancamento.tipo_pagamento);
     }
+    this.lancamentoEdit.setItems([]);
   }
 
   functionSelectTipoPagamento(e: any) {
@@ -177,5 +178,4 @@ export class NovoComponent implements OnInit {
   formatData(data: String) {
     return _formatData(data);
   }
-
 }
