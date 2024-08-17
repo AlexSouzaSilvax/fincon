4623593f36fbf4214646c4f12908d2aeda3cc092
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2024-08-16 23:16:41 -0300

    add esqueciSenha e register

--- src/app/fincon/services/usuario.service.ts
+++ src/app/fincon/services/usuario.service.ts
@@ -1,11 +1,10 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, first } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { first } from 'rxjs/operators';
 import { API } from '../../shared/Util';
 import { Usuario } from '../model/Usuario';
 import { UsuarioAccessDTO } from '../model/UsuarioAccessDTO';
-import { Observable } from 'rxjs/internal/Observable';
-import { of } from 'rxjs/internal/observable/of';
+import { UsuarioRegister } from '../model/UsuarioRegister';
 
 @Injectable({
   providedIn: 'root',
@@ -37,15 +36,27 @@ export class UsuarioService {
       //.pipe(first());
     } else {
       return this.httpClient
-        .post<Usuario>(`${API.url}/usuario/create`, record)
+        .post<Usuario>(`${API.url}/auth/register`, record)
         .toPromise();
       //.pipe(first());
     }
+  }
+
+  register(record: UsuarioRegister) {
+    return this.httpClient
+      .post<UsuarioRegister>(`${API.url}/auth/register`, record)
+      .toPromise();
   }
 
   delete(id: string) {
     return this.httpClient
       .post<String>(`${API.url}/usuario/delete`, id)
       .pipe(first());
+  }
+
+  esqueciSenha(record: String) {
+    return this.httpClient
+      .post<String>(`${API.url}/user/esqueci-senha`, record)
+      .toPromise();
   }
 }
