392582925e2ce3d1427ddc82a24805990a5b3285
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2025-01-21 16:32:45 -0300

    correcao server

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
+function requireHTTPS(req, res, next) {  
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }  
+  next();
+}
 const express = require("express");
-const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Serve arquivos estáticos da pasta 'build'
-app.use(express.static(path.join(__dirname, "build")));
-
-// Rota para todas as requisições
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.use(requireHTTPS);
+app.use(express.static("./dist/fincon"));
+app.get("/*", function (req, res) { 
+  res.set('Access-Control-Allow-Origin', '*');
+  res.sendFile("index.html", { root: "dist/fincon/" });
 });
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(process.env.PORT || 8080);
