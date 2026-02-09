2a8f4ff622643798662f49fe3e3b58aa733ea05d
Alex <44552810+AlexSouzaSilvax@users.noreply.github.com>
2026-02-09 09:36:01 -0300

    Fix conditional logic and loading state in principal component

--- src/app/fincon/screens/principal/principal.component.ts
+++ src/app/fincon/screens/principal/principal.component.ts
@@ -191,7 +191,7 @@ export class PrincipalComponent implements OnInit {
     var somaInvestimentosEntradas: any = 0;
     var somaInvestimentosSaidas: any = 0;
     for (var i = 0; i < lancamentos.length; i++) {
-      // if (lancamentos[i].pago) {
+       if (lancamentos[i].pago) {
         if (lancamentos[i].tipo_lancamento == 1) {
           //Saída
           somaSaidas += lancamentos[i].valor;
@@ -218,7 +218,7 @@ export class PrincipalComponent implements OnInit {
             somaInvestimentosEntradas += lancamentos[i].valor;
           }
         }
-     // }
+      }
     }
     this.totalEntrada$ = this.numberToReal(somaEntradas);
     this.totalSaida$ = this.numberToReal(somaSaidas);
@@ -283,6 +283,7 @@ export class PrincipalComponent implements OnInit {
   }
 
   onDelete(id: string) {
+    this.load = true;
     this.lancamentosService.delete(id).subscribe(
       (result) => {
         this.onSuccess('Apagado com sucesso');
@@ -291,6 +292,7 @@ export class PrincipalComponent implements OnInit {
         this.onError('Não foi possivel apagar este lançamento');
       }
     );
+    this.load = false;
   }
 
   novo() {
